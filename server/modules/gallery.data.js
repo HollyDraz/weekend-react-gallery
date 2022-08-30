5e65bbecb336a410bce54a20924b348159926867
Holly Drazenovich <hollydraze14@gmail.com>
2022-08-30 16:31:50 -0500

    added id and desc for all images thus far in the gallery.data.js file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/ghosts.jpg', description: 'Photo of three spooky figures standing in an orchard', likes: 0 },
+    { id: 3, path: 'images/bar.JPG', description: 'Photo of the outside of a bar-showing the neon lights', likes: 0 },
+    { id: 4, path: 'images/holly.jpg', description: 'Photo of me (holly) walking in a parking garage', likes: 0 },
+    { id: 5, path: 'images/house.jpg', description: 'Photo of the outside of a local house in minnesota', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
