a8948ad3fe430e047c9273747076e4e94b7919b6
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-03 09:57:33 -0500

    added use state and use effect as well as fetchgallery images to app function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-//import galleryItems from '../../../server/modules/gallery.data';
+//import { use } from '../../../server/routes/gallery.router';
+//import galleryItems from '../GalleryItem';
 
 
 
 function App() {
-    return (
+const [galleryItems, setGalleryItems] =useState([]);
+
+useEffect(() => {
+  console.log('use effect -page load');
+  fetchGalleryImages();
+}, []);
+
+const fetchGalleryImages = () => {
+  axios({
+    method: 'GET',
+    url: 'gallery'
+  }).then(response =>{
+    setGalleryItems(response.data);
+    console.log("get test", response.data );
+  }).catch(error =>{
+    console.log(error);
+    alert('something is wrong!')
+  });
+}
+  return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
         <br/>
-        
         <img src="images/ghosts.jpg"/>
         <br/>
         <img src="images/holly.JPG"/>
@@ -24,7 +44,7 @@ function App() {
       </div>
     );
 
-
+ 
 
 }
 
